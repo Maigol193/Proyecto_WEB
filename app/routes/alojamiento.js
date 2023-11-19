7dbce15acbb332df7c90586204f42b5499fbdeec
AlanMacias2002 <alenmaam@gmail.com>
2023-11-19 17:50:27 -0600

    mongoose

--- app/routes/alojamiento.js
+++ app/routes/alojamiento.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const { default: mongoose } = require('mongoose');
+let mongoConnection = "mongodb+srv://admin:admin@myapp.51n0kvh.mongodb.net/MyAppDB";
+let db = mongoose.connection;
 
 
 module.exports = router;
