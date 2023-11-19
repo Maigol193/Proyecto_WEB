d97d9e4a1c423dfdd7e92c1a8a86cba407702ae8
AlanMacias2002 <alenmaam@gmail.com>
2023-11-19 17:52:49 -0600

    mongoose

--- app/routes/alojamiento.js
+++ app/routes/alojamiento.js
@@ -4,5 +4,19 @@ const { default: mongoose } = require('mongoose');
 let mongoConnection = "mongodb+srv://admin:admin@myapp.51n0kvh.mongodb.net/MyAppDB";
 let db = mongoose.connection;
 
+app.use(express.json());
+
+db.on('connecting',()=>{
+    console.log('Conectando...');
+    console.log(mongoose.connection.readyState);
+});
+db.on('connected',()=>{
+    console.log('¡Conectado exitosamente!');
+    console.log(mongoose.connection.readyState);
+});
+
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 module.exports = router;
