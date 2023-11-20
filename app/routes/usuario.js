635ab257658c9fe672c12cbcf9468e99ee297fb3
Miguel <migueltorrespvr2@gmail.com>
2023-11-20 01:05:47 -0600

    rutas de usuarios hechas

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -41,37 +41,159 @@ const userSchema = mongoose.Schema({
 
 let Usuario = mongoose.model('usuarios',userSchema);
 
-router.get('/') //GET por email y correo
+//GET usuario por email y correo
+router.get('/user', (req,res) => {
+    let email = req.body.email,
+        password = req.body.password
+    
+    Usuario.find({
+        email: email,
+        password: password
+    }).then(function (docs) {
+        res.send(docs);
+        console.log(docs);
+    }).catch((err) => console.log(err));
+});
 
-router.post('/', (req,res) => {
+//POST usuario
+router.post('/create', (req,res) => {
     const newUser = req.body;
     const user = Usuario(newUser);
     //Guardamos el usuario en la BD (OJO, es asincrono)
     user.save().then((doc) => console.log("Usuario creado: ") + doc);
 });
 
-router.put('/') //PUT general, del account
-
-router.delete('/') //DELETE de alojamientos
-
-router.delete('/') //DELETE de reservaciones
-
-//Agregar alojamientos
-router.put('/add_alojamiento', (req,res) => {
+//DELETE alojamiento
+router.delete('/delete_alojamiento', (req,res) => {
+    let id = req.body.id,
+        toDelete = req.body.toDelete,
+        alojamientos = req.body.alojamientos,
+        object_to_update = {},
+        flag_updated = false;
     
+    if(toDelete != undefined){
+        const index = alojamientos.findIndex(alj => alj == toDelete);
+        if(index > -1){
+            alojamientos.splice(index, 1);
+            object_to_update.alojamientos = alojamientos;
+            flag_updated = true;
+        }
+    }
+
+    if(flag_updated){
+        Usuario.findByIdAndUpdate(id, object_to_update, {new: true}).then((doc) => {
+            console.log("Se ha removido el alojamiento");
+            console.log(doc);
+            res.send(doc);
+        }).catch((err) => console.log(err));
+    } else{
+        res.send("No se ha actualizado na'")
+    }
+});
+
+//DELETE reservación
+router.delete('/delete_reservacion', (req,res) => {
+    let id = req.body.id,
+        toDelete = req.body.toDelete,
+        reservations = req.body.reservations,
+        object_to_update = {},
+        flag_updated = false;
+    
+    if(toDelete != undefined){
+        const index = reservations.findIndex(rsv => rsv == toDelete);
+        if(index > -1){
+            reservations.splice(index, 1);
+            object_to_update.reservations = reservations;
+            flag_updated = true;
+        }
+    }
+
+    if(flag_updated){
+        Usuario.findByIdAndUpdate(id, object_to_update, {new: true}).then((doc) => {
+            console.log("Se ha removido la reservacion");
+            console.log(doc);
+            res.send(doc);
+        }).catch((err) => console.log(err));
+    } else{
+        res.send("No se ha actualizado na'")
+    }
+});
+
+//PUT del account
+router.put('/edit_account', (req,res) => {
+    let id = req.body.id,
+        name = req.body.name,
+        email = req.body.email,
+        cellphone = req.body.cellphone,
+        residencia = req.body.residencia,
+        description = req.body.description,
+        object_to_update = {},
+        flag_updated = false;
+
+    if(name != undefined){
+        object_to_update.name = name;
+        flag_updated = true;
+    }
+    if(email != undefined){
+        object_to_update.email = email;
+        flag_updated = true;
+    }
+    if(cellphone != undefined){
+        object_to_update.cellphone = cellphone;
+        flag_updated = true;
+    }
+    if(residencia != undefined){
+        object_to_update.residencia = residencia;
+        flag_updated = true;
+    }
+    if(description != undefined){
+        object_to_update.description = description;
+        flag_updated = true;
+    }
+
+    if(flag_updated){
+        Usuario.findByIdAndUpdate(id, object_to_update, {new: true}).then((doc) => {
+            console.log("Usuario actualizado");
+            console.log(doc);
+            res.send(doc);
+        }).catch((err) => console.log(err));
+    }
+    else{
+        res.send("No se ha actualizado na'")
+    }
+});
+
+//PUT de alojamientos
+router.put('/add_alojamiento', (req,res) => {
     let id = req.body.id,
         newAlojamiento = req.body.newAlojamiento,
         alojamientos = req.body.alojamientos,
         object_to_update = {},
         flag_updated = false;
     
-    console.log(id);
-    console.log(newAlojamiento);
-    console.log(alojamientos);
-    
     alojamientos.push(newAlojamiento);
-
     object_to_update.alojamientos = alojamientos;
+    flag_updated = true;
+
+    if(flag_updated){
+        Usuario.findByIdAndUpdate(id, object_to_update, {new: true}).then((doc) => {
+            console.log("Alojamientos actualizado");
+            console.log(doc);
+            res.send(doc);
+        }).catch((err) => console.log(err));
+    }
+});
+
+//PUT de reservaciones
+router.put('/add_reservacion', (req,res) => {
+    let id = req.body.id,
+        newReservacion = req.body.newReservacion,
+        reservations = req.body.reservations,
+        object_to_update = {},
+        flag_updated = false;
+    
+    reservations.push(newReservacion);
+    object_to_update.reservations = reservations;
     flag_updated = true;
 
     if(flag_updated){
