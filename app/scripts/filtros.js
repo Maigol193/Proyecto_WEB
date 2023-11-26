5e9ebbaeb38d659dfd342af93f50ff663e0d561b
AlanMacias2002 <alenmaam@gmail.com>
2023-11-26 12:06:49 -0600

    logeado_redirige

--- app/scripts/filtros.js
+++ app/scripts/filtros.js
@@ -1,41 +1,5 @@
 var alojamientosJSON;
 
-document.addEventListener("DOMContentLoaded", function () {
-    var htmlType = document.documentElement.getAttribute("data-html-type");
-
-    if (htmlType === "home-sin-log") {
-        console.log("Estás en la página 1.");
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://localhost:3000/sinAdmin/get_all', true);
-        xhr.send();
-
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                alojamientosJSON = JSON.parse(xhr.responseText);
-                alojamientosToDisplay(alojamientosJSON);
-            }
-            else {
-                alert(xhr.status + ": " + xhr.statusText);
-            }
-        };
-    } else if (htmlType === "home-log") {
-        console.log("Estás en la página 2.");
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://localhost:3000/sinAdmin/get_all', true);
-        xhr.send();
-
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                alojamientosJSON = JSON.parse(xhr.responseText);
-                alojamientosToDisplay(alojamientosJSON);
-            }
-            else {
-                alert(xhr.status + ": " + xhr.statusText);
-            }
-        };
-    }
-});
-
 function alojamientosToDisplay(array) {
     const alojamientos = array;
     let i = 1;
@@ -92,6 +56,98 @@ function alojamientosToDisplay(array) {
         });
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var htmlType = document.documentElement.getAttribute("data-html-type");
+
+    if (htmlType === "home-sin-log") {
+        console.log("Estás en la página 1.");
+        
+    } else if (htmlType === "home-log") {
+        console.log("Estás en la página 2.");
+        alojamientosToDisplay=function (array){
+            const alojamientos = array;
+    let i = 1;
+    let html = "<div class='row inline-flex grid grid-cols-4'>";
+    for (const alojamiento of alojamientos) {
+        html += `
+        <div class="card carta_margin flex flex-inline">
+        <div id="carouselId${i}" class="carousel slide" data-bs-ride="carousel" atribute_id_alojamiento="${alojamiento._id}">
+            <ol class="carousel-indicators">
+                <li data-bs-target="#carouselId${i}" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+                <li data-bs-target="#carouselId${i}" data-bs-slide-to="1" aria-label="Second slide"></li>
+                <li data-bs-target="#carouselId${i}" data-bs-slide-to="2" aria-label="Third slide"></li>
+                <li data-bs-target="#carouselId${i}" data-bs-slide-to="3" aria-label="Forth slide"></li>
+                <li data-bs-target="#carouselId${i}" data-bs-slide-to="4" aria-label="Fifth slide"></li>
+            </ol>
+            <div class="carousel-inner para_img" role="listbox">
+                <div class="carousel-item active">
+                    <img src="${alojamiento.images[0]}" class="w-100 d-block" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="${alojamiento.images[0]}" class="w-100 d-block" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="${alojamiento.images[0]}" class="w-100 d-block" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="${alojamiento.images[0]}" class="w-100 d-block" alt="Forth slide">
+                </div>
+                <div class="carousel-item">
+                    <img src=${alojamiento.images[0]}" class="w-100 d-block" alt="Fifth slide">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselId${i}" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselId${i}" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <div class="card-body">
+            <h4 class="card-title">${alojamiento.title}</h4>
+            <p class="card-text">${alojamiento.categories}  ${alojamiento.estado}</p>
+            <p class="flex items-center justify-between">
+                <b>Precio por noche</b>
+                <b>$${alojamiento.price}</b>
+            </p>
+            <p class="flex items-center justify-between">
+            <b></b>
+            <button type="button" class="btn btn-dark btn-sm items-center inline-flex rounded" id="primer-auth${i}"> Reservar &nbsp; <i class="fa-solid fa-arrow-right fa-sm" style="color: #ffffff;"></i> </button>
+            </p>
+        </div>
+    </div>
+            `
+        i++;
+    }
+    document.getElementById("inicio-body").innerHTML = html;
+    for (let c = 1; c < i; c++) {
+        document.getElementById("primer-auth" + c).addEventListener("click", function () {
+            var elemento = document.getElementById("carouselId"+c);
+            var valorAtributo = elemento.getAttribute('atribute_id_alojamiento');   
+            window.location.href = 'Pagina_reservando.html';
+        });
+    }
+        }
+    }
+});
+
+
+let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:3000/sinAdmin/get_all', true);
+        xhr.send();
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                alojamientosJSON = JSON.parse(xhr.responseText);
+                alojamientosToDisplay(alojamientosJSON);
+            }
+            else {
+                alert(xhr.status + ": " + xhr.statusText);
+            }
+        };
 
 var filtroCategorias = [];
 
