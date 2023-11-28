a408771a59a8ec9b3a011c78c7989adf0144f7df
AlanMacias2002 <alenmaam@gmail.com>
2023-11-27 20:39:26 -0600

    edit_alojamiento terminado

--- app/scripts/edit_alojamiento.js
+++ app/scripts/edit_alojamiento.js
@@ -1,9 +1,23 @@
 //const user = JSON.parse(sessionStorage.getItem("userData"));
 //var ID_cliente = user._id;
-var ID_alojamiento = "6564fada347911add6c3c062";
+var ID_alojamiento = "656533ff3688b2aeea93b24b";
 var ID_cliente = "6564fada347911add6c3c062";
-console.log(ID_cliente);
 
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://localhost:3000/alojamientos/get_by_id?_id=' + ID_alojamiento, true);
+xhr.setRequestHeader("x-auth", "admin");
+xhr.send();
+
+xhr.onload = function () {
+    if (xhr.status == 200) {
+        var alojamientoJSON = JSON.parse(xhr.responseText);
+        passTodo(alojamientoJSON);
+        console.log(alojamientoJSON)
+    }
+    else {
+        alert(xhr.status + ": " + xhr.statusText);
+    }
+};
 
 let xhr2 = new XMLHttpRequest();
 xhr2.open('GET', 'http://localhost:3000/usuarios/get_by_id?_id=' + ID_cliente, true);
@@ -13,7 +27,6 @@ xhr2.send();
 xhr2.onload = function () {
     if (xhr2.status == 200) {
         var NovoUsuario = JSON.parse(xhr2.responseText);
-        console.log(NovoUsuario);
         console.log(NovoUsuario[0]._id);
         Pass_host_name_to_html(NovoUsuario);
     }
@@ -40,22 +53,53 @@ function cambiarTexto(nuevoTexto) {
     dropdownButton.innerHTML = nuevoTexto;
 }
 
-function updateUserDataInStorage(){
-    let xhr2 = new XMLHttpRequest();
-    xhr2.open('GET', 'http://localhost:3000/usuarios/get_by_id?_id=' + ID_cliente, true);
-    xhr2.setRequestHeader("x-auth", "admin");
-    xhr2.send();
+function passTodo(array){
+    const alojamientos = array;
+    console.table(array);
+    for (const alojamiento of alojamientos) {
+        var Titulo_html = document.getElementById("Titulo");
+        var Ubicacion_html = document.getElementById('Ubicacion');
+        var Banos_html = document.getElementById('Banos');
+        var Habitaciones_html = document.getElementById('Habitaciones');
+        var Huespedes_html = document.getElementById('Huespedes');
+        var Camas_html = document.getElementById('Camas');
+        var Price_html = document.getElementById('Price');
+        var Img1_html = document.getElementById('Img1');
+        var Img2_html = document.getElementById('Img2');
+        var Img3_html = document.getElementById('Img3');
+        var Img4_html = document.getElementById('Img4');
+        var Img5_html = document.getElementById('Img5');
+        var Description_html = document.getElementById('Descripcion');
+        var dropdownButton = document.getElementById("dropdown-button");
+        Titulo_html.value = alojamiento.title;
+        Ubicacion_html.value = alojamiento.ubicacion;
+        Banos_html.value = alojamiento.banos;
+        Habitaciones_html.value = alojamiento.rooms;
+        Huespedes_html.value = alojamiento.huespedes;
+        Camas_html.value = alojamiento.beds;
+        Price_html.value = alojamiento.price;
+        Img1_html.value = alojamiento.images[0];
+        Img2_html.value = alojamiento.images[1];
+        Img3_html.value = alojamiento.images[2];
+        Img4_html.value = alojamiento.images[3];
+        Img5_html.value = alojamiento.images[4];
+        Description_html.value = alojamiento.description;
+        dropdownButton.innerHTML = alojamiento.estado;
 
-    xhr2.onload = function () {
-        if (xhr2.status == 200) {
-            sessionStorage.removeItem('userData');
-            sessionStorage.setItem('userData', xhr2.responseText);
+        var ids = alojamiento.categories;
+
+        ids.forEach(function (id) {
+        var checkbox = document.getElementById(id);
+
+        if (checkbox) {
+            checkbox.checked = true;
         }
-        else {
-            alert(xhr2.status + ": " + xhr2.statusText);
-        }
-    };
+    });
+    }
+
 }
+
+
 
 function printTodo() {
 
@@ -86,6 +130,7 @@ function printTodo() {
     var Img4Value = Img4_html.value;
     var Img5Value = Img5_html.value;
 
+
     // Obtener todas las checkbox dentro del ul con id "Category_checkbox"
     var checkboxes = document.querySelectorAll('.pl');
 
@@ -107,7 +152,7 @@ function printTodo() {
     images_array.push(Img5Value);
 
     let AloData = {
-        host:ID_cliente,
+        id: ID_alojamiento,
         title: TituloValue,
         ubicacion: UbicacionValue,
         description: DescripcionValues,
@@ -116,29 +161,25 @@ function printTodo() {
         huespedes: HuespedesValue,
         rooms: HabitacionesValue,
         categories: idsSeleccionados,
-        images: ["imagen1.png","imagen1.png","imagen1.png","imagen1.png","imagen1.png"],
+        images: images_array,
         price: PriceValue,
         estado: filtroEstado,
-        reservaciones: []
     };
 
     console.log(AloData);
-
-    /*
     let AloDataJSON = JSON.stringify(AloData);
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/alojamientos/create');
+    xhr.open('PUT', 'http://localhost:3000/alojamientos/edit_alojamiento');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader("x-auth", "admin");
     xhr.send(AloDataJSON);
     xhr.onload = function () {
         if (xhr.status === 200) {
-            console.log("Alojamiento creado con éxito");
-            updateUserDataInStorage();
+            console.log("Alojamiento editado con éxito");
         } else {
-            console.error("Error al crear el alojamiento");
+            console.error("Error al editar el alojamiento");
         }
-    };*/
+    };
 }
 
 
